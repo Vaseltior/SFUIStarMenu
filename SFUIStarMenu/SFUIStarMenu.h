991ababf97d0c49d457150a6db14bad34c01e64a
Samuel GRAU <samuel.grau@gmail.com>
2015-03-13 19:41:35 +0100

    Initial Commit

---
+++ SFUIStarMenu/SFUIStarMenu.h
@@ -0,0 +1,19 @@
+//
+//  SFUIStarMenu.h
+//  SFUIStarMenu
+//
+//  Created by Samuel Grau on 13/03/2015.
+//  Copyright (c) 2015 Samuel GRAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFUIStarMenu.
+FOUNDATION_EXPORT double SFUIStarMenuVersionNumber;
+
+//! Project version string for SFUIStarMenu.
+FOUNDATION_EXPORT const unsigned char SFUIStarMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFUIStarMenu/PublicHeader.h>
+
+
